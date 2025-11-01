5b92a087db5246a3984583510de3c25367df1374
mahdi kashani <esmokes17@gmail.com>
2025-11-01 18:18:57 +0330

    [User] Refresh token API

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.views import my_user_profile_view, login_view, signup_view, user_profile_view
 
@@ -6,6 +7,7 @@ from users.views import my_user_profile_view, login_view, signup_view, user_prof
 urlpatterns = [
     path("login", login_view, name="login"),
     path("signup", signup_view, name="signup"),
+    path("refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("me", my_user_profile_view, name="my profile"),
     path("<int:user_id>", user_profile_view, name="my profile"),
 ]
