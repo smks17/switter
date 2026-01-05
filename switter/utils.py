de96ac4f48f41c92470875a2458f2517d7d70e7d
mahdi kashani <esmokes17@gmail.com>
2026-01-06 00:30:23 +0330

    [Switter] bugfix caching

--- switter/utils.py
+++ switter/utils.py
@@ -1,28 +1,31 @@
+import json
 from django.core.cache import cache
+from rest_framework.response import Response
 from functools import wraps
 
 
 def create_user_cache(user_id, request_path):
-    return f"{user_id}:{request_path}"
+    return f"user:{user_id}:{request_path}"
 
 
 def user_cached(timeout=300):
     def decorator(view_func):
         @wraps(view_func)
-        def wrapper(request, *args, **kwargs):
+        def wrapper(obj, request, *args, **kwargs):
             user = getattr(request, "user", None)
             if not user or not user.is_authenticated:
-                return view_func(request, *args, **kwargs)
+                return view_func(obj, request, *args, **kwargs)
 
-            cache_key = create_user_cache(user.id, request.path)
+            cache_key = create_user_cache(user.id, request.get_full_path())
             if request.GET.get("reload") != "true":
                 cached_result = cache.get(cache_key)
                 if cached_result:
-                    return cached_result
+                    return Response(json.loads(cached_result))
 
-            response = view_func(request, *args, **kwargs)
+            response = view_func(obj, request, *args, **kwargs)
 
-            cache.set(cache_key, response, timeout=timeout)
+            if response.status_code == 200:
+                cache.set(cache_key, json.dumps(response.data), timeout=timeout)
 
             return response
 
