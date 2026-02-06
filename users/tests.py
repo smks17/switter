b6bcbc383d89d7a9136ba577e7efe721a5b6743a
mahdi kashani <esmokes17@gmail.com>
2026-02-06 23:28:43 +0330

    [Users] Basic unittest

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(
+            username="testuser", email="test@example.com", password="testpassword123"
+        )
+        self.signup_url = reverse("signup")
+        self.login_url = reverse("login")
+
+    def test_signup_user(self):
+        data = {
+            "username": "newuser",
+            "email": "new@example.com",
+            "password": "newpassword123",
+        }
+        response = self.client.post(self.signup_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(User.objects.filter(username="newuser").exists())
+
+    def test_login_user(self):
+        data = {"username": "testuser", "password": "testpassword123"}
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
